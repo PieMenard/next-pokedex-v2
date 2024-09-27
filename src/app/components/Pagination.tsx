4596e272f2a7f279bed9dcf6c8a5ff5f1d1a1c14
PieMenard <pinedo.m.gustavo@gmail.com>
2024-09-27 19:04:09 -0400

    merge

--- src/app/components/Pagination.tsx
+++ src/app/components/Pagination.tsx
@@ -1,17 +1,4 @@
 type PaginationProps = {
-<<<<<<< HEAD
-  setOffset: (n: number) => void;
-  offset: number;
-  limit: number;
-  totalResults: number;
-};
-
-const Pagination = ({
-  setOffset,
-  offset,
-  limit,
-  totalResults,
-=======
   offset: number;
   limit: number;
   totalResults: number;
@@ -25,28 +12,19 @@ const Pagination = ({
   totalResults,
   setOffset,
   loading,
->>>>>>> fd3a74e2ff3d1432095a08f61aa55781fd2d5734
 }: PaginationProps) => {
   return (
     <div className="my-4">
       <button
         onClick={() => setOffset(offset - limit)}
-<<<<<<< HEAD
-        disabled={offset === 0}
-=======
         disabled={offset === 0 || loading}
->>>>>>> fd3a74e2ff3d1432095a08f61aa55781fd2d5734
         className="mx-4 bg-teal-800 text-white rounded-lg disabled:opacity-40 px-2"
       >
         Prev
       </button>
       <button
         onClick={() => setOffset(offset + limit)}
-<<<<<<< HEAD
-        disabled={offset >= totalResults - limit}
-=======
         disabled={offset >= totalResults - limit || loading}
->>>>>>> fd3a74e2ff3d1432095a08f61aa55781fd2d5734
         className="mx-4 bg-teal-800 text-white rounded-lg disabled:opacity-40 px-2"
       >
         Next
