1e35ed3797437a45194b51f56b29a040bcbfadb4
PieMenard <110061246+PieMenard@users.noreply.github.com>
2024-09-27 10:27:31 -0400

    add loading message

--- src/app/components/Pagination.tsx
+++ src/app/components/Pagination.tsx
@@ -3,6 +3,7 @@ type PaginationProps = {
   limit: number;
   totalResults: number;
   setOffset: (n: number) => void;
+  loading: boolean;
 };
 
 const Pagination = ({
@@ -10,19 +11,20 @@ const Pagination = ({
   limit,
   totalResults,
   setOffset,
+  loading,
 }: PaginationProps) => {
   return (
     <div className="my-4">
       <button
         onClick={() => setOffset(offset - limit)}
-        disabled={offset === 0}
+        disabled={offset === 0 || loading}
         className="mx-4 bg-teal-800 text-white rounded-lg disabled:opacity-40 px-2"
       >
         Prev
       </button>
       <button
         onClick={() => setOffset(offset + limit)}
-        disabled={offset >= totalResults - limit}
+        disabled={offset >= totalResults - limit || loading}
         className="mx-4 bg-teal-800 text-white rounded-lg disabled:opacity-40 px-2"
       >
         Next
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,28 +3,35 @@
 import { Pokemon } from '@/types/Pokemon';
 import { useEffect, useState } from 'react';
 import Pagination from './components/Pagination';
-import { off } from 'process';
 
 export default function Home() {
   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
   const limit = 20;
   const [totalResults, setTotalResults] = useState(0);
   const [offset, setOffset] = useState(0);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchPokemonList = async () => {
-      const res = await fetch(
-        `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
-      );
-      const data = await res.json();
-      setTotalResults(data.count);
+      setLoading(true);
+      try {
+        const res = await fetch(
+          `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
+        );
+        const data = await res.json();
+        setTotalResults(data.count);
 
-      const fetchPokemonDetails = data.results.map(async (pokemon: any) => {
-        const res = await fetch(pokemon.url);
-        return res.json();
-      });
-      const pokemonDetails = await Promise.all(fetchPokemonDetails);
-      setPokemons(pokemonDetails);
+        const fetchPokemonDetails = data.results.map(async (pokemon: any) => {
+          const res = await fetch(pokemon.url);
+          return res.json();
+        });
+        const pokemonDetails = await Promise.all(fetchPokemonDetails);
+        setPokemons(pokemonDetails);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
     };
     fetchPokemonList();
   }, [offset]);
@@ -33,19 +40,24 @@ export default function Home() {
     <main className="flex flex-col items-center">
       <h1 className="font-bold my-5 text-2xl text-teal-800">PokeDex v2</h1>
       <div>
-        <ul>
-          {pokemons.map((pokemon: Pokemon) => (
-            <li key={pokemon.id}>
-              {pokemon.id}. {pokemon.name}
-            </li>
-          ))}
-        </ul>
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          <ul>
+            {pokemons.map((pokemon: Pokemon) => (
+              <li key={pokemon.id}>
+                {pokemon.id}. {pokemon.name}
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
       <Pagination
         limit={limit}
         offset={offset}
         totalResults={totalResults}
         setOffset={setOffset}
+        loading={loading}
       />
     </main>
   );
