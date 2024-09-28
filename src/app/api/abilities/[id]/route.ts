e19c847734e1c59b08c6cfaf018ad5cc322ba7b4
PieMenard <pinedo.m.gustavo@gmail.com>
2024-09-28 00:17:03 -0400

    implement endpoint to update ability

--- src/app/api/abilities/[id]/route.ts
+++ src/app/api/abilities/[id]/route.ts
@@ -18,3 +18,27 @@ export async function GET(req: NextRequest) {
     return NextResponse.json({ success: false, error: error });
   }
 }
+
+export async function PUT(req: NextRequest) {
+  try {
+    const id = parseInt(req.url.split('/abilities/')[1]);
+    const newData = await req.json();
+
+    if (newData.id) {
+      return NextResponse.json({ success: false, error: 'Cannot update id' });
+    }
+
+    const ability = await prisma.ability.update({
+      where: { id },
+      data: {
+        is_main_series: newData.is_main_series,
+        language: newData.language,
+        name: newData.name,
+      },
+    });
+
+    return NextResponse.json({ success: true, data: ability });
+  } catch (error) {
+    return NextResponse.json({ success: false, error: error });
+  }
+}
