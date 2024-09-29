17a1ca3f380dfc968f144954cb9f06ffa181ddab
PieMenard <pinedo.m.gustavo@gmail.com>
2024-09-29 02:21:18 -0400

    implement endpoint to search by criteria

--- src/app/api/pokemon/route.ts
+++ src/app/api/pokemon/route.ts
@@ -33,7 +33,40 @@ export async function GET(req: NextRequest) {
     const { searchParams } = new URL(req.url);
     const offset = parseInt(searchParams.get('offset') || '0');
     const limit = parseInt(searchParams.get('limit') || '10');
+
+    const idSearch = searchParams.get('id');
+    const weightSearch = searchParams.get('weight');
+    const nameSearch = searchParams.get('name');
+    const abilitySearch = searchParams.get('ability');
+    const heightSearch = searchParams.get('height');
+
+    const filters: any = {};
+
+    if (idSearch) {
+      filters.id = parseInt(idSearch);
+    }
+    if (weightSearch) {
+      filters.weight = parseInt(weightSearch);
+    }
+    if (nameSearch) {
+      filters.name = { contains: nameSearch, mode: 'insensitive' };
+    }
+    if (abilitySearch) {
+      filters.abilities = {
+        some: {
+          name: {
+            contains: abilitySearch,
+            mode: 'insensitive',
+          },
+        },
+      };
+    }
+    if (heightSearch) {
+      filters.height = parseInt(heightSearch);
+    }
+
     const pokemonList = await prisma.pokemon.findMany({
+      where: filters,
       select: {
         name: true,
         id: true,
@@ -41,11 +74,19 @@ export async function GET(req: NextRequest) {
       take: limit,
       skip: offset,
     });
+
+    if (pokemonList.length === 0) {
+      return NextResponse.json(
+        { success: false, message: 'No pokemon found' },
+        { status: 404 }
+      );
+    }
     const results = {
       offset: offset,
       limit: limit,
       results: pokemonList,
     };
+
     return NextResponse.json({ success: true, data: results });
   } catch (error) {
     return NextResponse.json({ success: false, error: error });
